493b8373ca4a4822305c594112eea2351049aa9f
bbhhoonn <4025jbh@naver.com>
2021-05-03 11:01:14 +0900

    javascript

---
+++ javascript/user.js
@@ -0,0 +1,174 @@
+var user = [
+    {
+      "_id": "608cea5b5cd3be806aa73bad",
+      "age": 36,
+      "name": "Robert Solis",
+      "gender": "female",
+      "company": "QUINTITY",
+      "email": "robertsolis@quintity.com",
+      "phone": "+1 (916) 447-2460"
+    },
+    {
+      "_id": "608cea5b82615e160ec9d099",
+      "age": 30,
+      "name": "Dora Torres",
+      "gender": "female",
+      "company": "RETRACK",
+      "email": "doratorres@retrack.com",
+      "phone": "+1 (936) 538-2316"
+    },
+    {
+      "_id": "608cea5b7e4eef231b47e901",
+      "age": 39,
+      "name": "Jolene Hall",
+      "gender": "female",
+      "company": "ZILLAN",
+      "email": "jolenehall@zillan.com",
+      "phone": "+1 (917) 438-3835"
+    },
+    {
+      "_id": "608cea5b681f74721e310eff",
+      "age": 31,
+      "name": "Byers Kent",
+      "gender": "male",
+      "company": "QUARMONY",
+      "email": "byerskent@quarmony.com",
+      "phone": "+1 (914) 536-2392"
+    },
+    {
+      "_id": "608cea5b9920247732f26125",
+      "age": 35,
+      "name": "Esperanza Ramos",
+      "gender": "female",
+      "company": "ROCKLOGIC",
+      "email": "esperanzaramos@rocklogic.com",
+      "phone": "+1 (958) 454-2649"
+    },
+    {
+      "_id": "608cea5bee0885036295832f",
+      "age": 33,
+      "name": "Christy Dean",
+      "gender": "female",
+      "company": "PROGENEX",
+      "email": "christydean@progenex.com",
+      "phone": "+1 (993) 474-2526"
+    },
+    {
+      "_id": "608cea5bb98115cda2353fa0",
+      "age": 38,
+      "name": "Wise Kirby",
+      "gender": "male",
+      "company": "ISODRIVE",
+      "email": "wisekirby@isodrive.com",
+      "phone": "+1 (942) 505-3027"
+    },
+    {
+      "_id": "608cea5bd5e843189f30cca6",
+      "age": 20,
+      "name": "Salinas Mcmillan",
+      "gender": "male",
+      "company": "ENERFORCE",
+      "email": "salinasmcmillan@enerforce.com",
+      "phone": "+1 (980) 507-3191"
+    },
+    {
+      "_id": "608cea5b159ee3a42c9d7f03",
+      "age": 34,
+      "name": "Julie Cameron",
+      "gender": "female",
+      "company": "VIOCULAR",
+      "email": "juliecameron@viocular.com",
+      "phone": "+1 (872) 512-3596"
+    },
+    {
+      "_id": "608cea5beb175eca9ada74f0",
+      "age": 27,
+      "name": "Burch Guerrero",
+      "gender": "male",
+      "company": "OVOLO",
+      "email": "burchguerrero@ovolo.com",
+      "phone": "+1 (848) 452-2354"
+    },
+    {
+      "_id": "608cea5bb29888c5d386a3d9",
+      "age": 30,
+      "name": "Thelma Graves",
+      "gender": "female",
+      "company": "UNISURE",
+      "email": "thelmagraves@unisure.com",
+      "phone": "+1 (805) 551-2212"
+    },
+    {
+      "_id": "608cea5bed1aa6f097fb447a",
+      "age": 33,
+      "name": "Robyn Dorsey",
+      "gender": "female",
+      "company": "EXTRAWEAR",
+      "email": "robyndorsey@extrawear.com",
+      "phone": "+1 (979) 500-3941"
+    },
+    {
+      "_id": "608cea5b026b76c01addcd6c",
+      "age": 39,
+      "name": "Anastasia Cross",
+      "gender": "female",
+      "company": "PLASMOSIS",
+      "email": "anastasiacross@plasmosis.com",
+      "phone": "+1 (943) 450-2009"
+    },
+    {
+      "_id": "608cea5b4de3ce174af400d7",
+      "age": 36,
+      "name": "Chelsea Osborn",
+      "gender": "female",
+      "company": "ZILODYNE",
+      "email": "chelseaosborn@zilodyne.com",
+      "phone": "+1 (997) 409-3103"
+    },
+    {
+      "_id": "608cea5b9e84de9fcbf39d23",
+      "age": 21,
+      "name": "Jones Simmons",
+      "gender": "male",
+      "company": "VIASIA",
+      "email": "jonessimmons@viasia.com",
+      "phone": "+1 (916) 593-2995"
+    },
+    {
+      "_id": "608cea5bb0b1aeb9c8c6013e",
+      "age": 22,
+      "name": "Webb Prince",
+      "gender": "male",
+      "company": "HOUSEDOWN",
+      "email": "webbprince@housedown.com",
+      "phone": "+1 (935) 521-2522"
+    },
+    {
+      "_id": "608cea5b7963449d2c4f6d3b",
+      "age": 30,
+      "name": "York Cardenas",
+      "gender": "male",
+      "company": "PYRAMI",
+      "email": "yorkcardenas@pyrami.com",
+      "phone": "+1 (942) 497-3284"
+    },
+    {
+      "_id": "608cea5b0a96cd0ca6f643cd",
+      "age": 33,
+      "name": "Paula Valdez",
+      "gender": "female",
+      "company": "UNEEQ",
+      "email": "paulavaldez@uneeq.com",
+      "phone": "+1 (827) 442-2855"
+    },
+    {
+      "_id": "608cea5bbe3d543b305f7da6",
+      "age": 35,
+      "name": "Boyle Kidd",
+      "gender": "male",
+      "company": "EYEWAX",
+      "email": "boylekidd@eyewax.com",
+      "phone": "+1 (868) 467-2679"
+    }
+  ]
+  
